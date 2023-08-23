063a6c977b26a4f321fc9e9055070a12cb70cd1c
Tan <nitiwit2011@gmail.com>
2023-08-23 16:40:39 +0700

    Initial commit

---
+++ src/main/java/com/example/apitest/controller/ApiController.java
@@ -0,0 +1,30 @@
+package com.example.apitest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.apitest.model.ApiModel;
+import com.example.apitest.service.ApiService;
+
+@RestController
+@RequestMapping("apitest")
+public class ApiController {
+
+    @Autowired
+    private ApiService apiservice;
+
+    // @GetMapping("/findAll")
+    // public Object findAll() {
+    //     return apiservice.findAll();
+    // }
+
+    @PostMapping("/gentoken ")
+    public String insert(@RequestBody ApiModel apiModel) {
+        return apiservice.insert(apiModel);
+    }
+}
